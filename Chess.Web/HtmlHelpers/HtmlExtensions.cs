88280ca30546f259af167eec6444ca2f319a8af5
nikolay-spasov <nikolay.spasov.bg@gmail.com>
2015-03-08 15:49:16 +0200

    Reconfigured build

---
+++ Chess.Web/HtmlHelpers/HtmlExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Chess.Web.HtmlHelpers
+{
+    using System.Web.Mvc;
+
+    public static class HtmlExtensions
+    {
+        public static bool IsDebug(this HtmlHelper html)
+        {
+#if DEBUG
+            return true;
+#endif
+            return false;
+        }
+    }
+}
